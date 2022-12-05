bbcb223e9b28c58f561ae6bde9cd908891df9223
이동언 <80028638+47813@users.noreply.github.com>
2022-12-05 22:15:28 +0900

    Update operations.c
    Added comments, because of the readability of the file.

--- operations.c
+++ operations.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
     
-    double a = 4.0, b = 1.2;
+    double a = 4.0, b = 1.2;    //Declare 2 variables and values
     
     printf("%.1f + %.1f = %.1f \n", a, b, a+b);
     printf("%.1f - %.1f = %.1f \n", a, b, a-b);
     printf("%.1f * %.1f = %.1f \n", a, b, a*b);
-    printf("%.1f / %.1f = %.1f \n", a, b, a/b);
+    printf("%.1f / %.1f = %.1f \n", a, b, a/b); //Print each result of operators
     
     return 0;
 }
