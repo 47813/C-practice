7cdd0862b6471bd56e034511396af2db7c78782f
이동언 <80028638+47813@users.noreply.github.com>
2022-12-05 22:09:52 +0900

    Added operations.c
    This file practices four arithmetic operators in C.

---
+++ operations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    
+    double a = 4.0, b = 1.2;
+    
+    printf("%.1f + %.1f = %.1f \n", a, b, a+b);
+    printf("%.1f - %.1f = %.1f \n", a, b, a-b);
+    printf("%.1f * %.1f = %.1f \n", a, b, a*b);
+    printf("%.1f / %.1f = %.1f \n", a, b, a/b);
+    
+    return 0;
+}
